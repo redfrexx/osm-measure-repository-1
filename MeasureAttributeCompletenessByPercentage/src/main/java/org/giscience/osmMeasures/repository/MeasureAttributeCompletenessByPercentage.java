451300f88811c0ef762eb71bce863ff32f46ac1c
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2019-02-12 10:14:54 +0100

    speed upcode

--- MeasureAttributeCompletenessByPercentage/src/main/java/org/giscience/osmMeasures/repository/MeasureAttributeCompletenessByPercentage.java
+++ MeasureAttributeCompletenessByPercentage/src/main/java/org/giscience/osmMeasures/repository/MeasureAttributeCompletenessByPercentage.java
@@ -106,10 +106,7 @@ public class MeasureAttributeCompletenessByPercentage extends MeasureOSHDB<Numbe
         }
         **/
         SortedMap<GridCell, Integer> res = mapReducer.osmType(OSMType.WAY).osmTag("building").count();
-        SortedMap<GridCell, Number> cast_res = Cast.result(mapReducer
-            //.filter(x -> has_all_tags(x.getEntity(), baseTags, tagTranslator))
-            //.map(x -> getTagCoverage(x.getEntity(), subTags, tagTranslator))
-            .count());
+        SortedMap<GridCell, Number> cast_res = Cast.result(res);
         System.out.println("stop");
         
         // Compute percentage of tag completeness for each feature
